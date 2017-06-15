fb0ba86c7c075fdd298386f82cdb0a6e7b873a69
Knastordin <uxx_zz@mail.ru>
2017-06-15 17:36:50 +0200

    Added CRUD for Phones

--- Raer_Camp_Project_DEMO/Raer_Camp_Project_DEMO/Controllers/HomeController.cs
+++ Raer_Camp_Project_DEMO/Raer_Camp_Project_DEMO/Controllers/HomeController.cs
@@ -20,24 +20,73 @@ namespace Raer_Camp_Project_DEMO.Controllers
             return View(db.Phones.ToList());
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
+        [HttpGet]
+        public IActionResult Create() {
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+        [HttpPost]
+        public async Task<string> Create([Bind("Name","Company","Price")]Phone phone) {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Phones.Add(phone);
+                    await db.SaveChangesAsync();
+                    return "Phone " + phone.Name + " " + phone.Company + " was created";
+                }
+                else return "All fields must be filled";
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
-        public IActionResult Error()
-        {
-            return View();
+        [HttpGet]
+        public IActionResult Edit(int id) {
+            if (id != 0)
+            {
+                var phone = db.Phones.SingleOrDefault(x => x.Id == id);
+                return View(phone);
+            }
+            else return View();
         }
+
+        [HttpPost]
+        public async Task<string> Edit([Bind("Id", "Name", "Company", "Price")]Phone phone) {
+            try {
+                if (ModelState.IsValid)
+                {
+                    db.Phones.Update(phone);
+                    await db.SaveChangesAsync();
+                    return "Phone " + phone.Name + " " + phone.Company + " was edited";
+                }
+                else return "All fields must be filled";
+            }
+            catch (Exception ex) {
+                return ex.Message;
+            }
+        }
+
+        [HttpPost,HttpGet]
+        public async Task<IActionResult> Delete(int id) {
+            try {
+                if (id != 0)
+                {
+                    var currentPhone = db.Phones.SingleOrDefault(x => x.Id == id);
+                    db.Phones.Remove(currentPhone);
+                    await db.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex) {
+                ViewBag.Error = ex.Message;
+            }
+
+            return View("Index", db.Phones.ToList());
+
+        }
+       
 
         [HttpGet]
         public IActionResult Buy(int id) {
